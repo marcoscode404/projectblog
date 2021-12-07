970ae5fa50ec54da61ed54a876578c7ca97c1ca7
Marcos Vini/trabalho <viniciusmg99@outlook.com>
2021-12-07 05:51:55 -0400

    adicionando javascript do menu responsivo e alterando css

--- main.js
+++ main.js
@@ -46,3 +46,26 @@ function showSlides(n) {
 // }
 
 
+
+
+// botão do menu
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+
+  // controla acessibilidade para deficientes visuais
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+  
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
